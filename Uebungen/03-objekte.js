af6d3241f0fd6b4d638f0054a800a2615040abb2
Jan Hellerhoff <156646851+hj156666@users.noreply.github.com>
2024-09-23 07:08:35 +0000

    ...

---
+++ Uebungen/03-objekte.js
@@ -0,0 +1,64 @@
+console.log("Oblekte")
+console.log("=======")
+
+//
+//
+//
+//
+//
+//
+class Schueler{
+    constructor(){
+        this.Vorname
+        this.Nachname
+        this.Klasse
+    }
+}
+
+//
+
+let Schueler = new Schueler()
+    schueler.Vorname = "Hans"
+    schueler.Nachname = "Meyer"
+    schueler.Klasse = "GW23A"
+
+    console.log("Nachname: " + schueler.Nachname)
+
+let schueler2 = new Schueler()
+schueler2.Nachname = "Müller"
+schueler2.Vorname = "Pit"
+schueler2.Klasse = "GW23A"
+
+console.log("Aufgabe 1")
+
+let Auto{
+    this. Preis
+    this. Farbe
+    this. Baujahr
+    }
+
+let borAb123 = new Auto();
+let borXy876 = new Auto();
+let bohQz253 = new Auto();
+
+borAb123.Preis = 30000
+borAb123.Baujahr = 2000
+borAb123.Farbe = "blau"
+
+console.log("Preis: " + borAb123.Preis + ", Baujahr: " +borAb123.Baujahr + " , Farbe: " + borAb123.Farbe)
+
+console.log("Aufgabe 2")
+
+class Fahrrad{
+constructor(){
+    this. typ
+    this. Farbe
+    this. Preis
+    }
+
+    let fahrrad = new Fahrrad()
+    fahrrad.Typ = "Mountainbike"
+    fahrrad.Preis = 1000
+    fahrrad.Farbe = "rot"
+
+    console.log("Typ: " + fahrrad.Typ + ",Preis")
